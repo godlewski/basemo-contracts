2f1791bf436f1b82c976d40fc779d216ba009722
Steve Godlewski <steve.w.godlewski@gmail.com>
2024-12-16 20:03:55 -0700

    POC: testing out upgradeable contract

---
+++ script/UpgradeToV2.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/Basemo.sol";
+import "../src/BasemoV2.sol";
+
+contract UpgradeToV2 is Script {
+    function run() external {
+        address proxyAddress = vm.envAddress("PROXY_ADDRESS");
+
+        vm.startBroadcast();
+
+        // 1. Deploy new implementation
+        BasemoV2 implementationV2 = new BasemoV2();
+
+        // 2. Upgrade proxy to V2
+        Basemo proxy = Basemo(proxyAddress);
+        proxy.upgradeTo(address(implementationV2));
+
+        // 3. Set initial max debt amount
+        BasemoV2(proxyAddress).setMaxDebtAmount(1000 ether);
+
+        console.log(
+            "V2 Implementation deployed to:",
+            address(implementationV2)
+        );
+        console.log("Proxy upgraded to V2");
+
+        vm.stopBroadcast();
+    }
+}
